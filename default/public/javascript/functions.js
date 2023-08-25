11fb3b2aa6a158512d51b3b96c8cab694bbd78a7
AlfredoVientoGonzalez <alfredo.gonzalez@vientodigital.com>
2023-08-25 03:10:54 -0600

    Finalizando Cambios

--- default/public/javascript/functions.js
+++ default/public/javascript/functions.js
@@ -4,7 +4,15 @@ function clearInputs ()
     $('input[type=number]').each( (i,input) => $(input).val(null) );
 }
 
-$(document).ready(function() {
-    $('li.active').removeClass('active').removeAttr('aria-current');
-    $('a[href="' + location.pathname + '"]').closest('li').addClass('active').attr('aria-current', 'page'); 
-  });
+if(window.location.href.includes('localhost')){
+    $(document).ready(function() {
+        $('li.active').removeClass('active').removeAttr('aria-current');
+        $('a[href="' + location.pathname + '"]').closest('li').addClass('active').attr('aria-current', 'page'); 
+      });
+}
+else{
+    $(document).ready(function() {
+        $('li.active').removeClass('active').removeAttr('aria-current');
+        $('a[href="' + window.location.href + '"]').closest('li').addClass('active').attr('aria-current', 'page'); 
+      });
+    }
