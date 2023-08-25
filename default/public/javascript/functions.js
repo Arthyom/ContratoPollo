5afe67f23e2684cf1d6009210d098270aa19ad6e
AlfredoVientoGonzalez <alfredo.gonzalez@vientodigital.com>
2023-08-25 02:43:19 -0600

    Arreglando js

--- default/public/javascript/functions.js
+++ default/public/javascript/functions.js
@@ -4,3 +4,7 @@ function clearInputs ()
     $('input[type=number]').each( (i,input) => $(input).val(null) );
 }
 
+$(document).ready(function() {
+    $('li.active').removeClass('active').removeAttr('aria-current');
+    $('a[href="' + location.pathname + '"]').closest('li').addClass('active').attr('aria-current', 'page'); 
+  });
