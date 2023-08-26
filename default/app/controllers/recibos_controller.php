1bbe763258ad71c55d63605f6a3156e1ffcaa23c
AlfredoVientoGonzalez <alfredo.gonzalez@vientodigital.com>
2023-08-25 22:52:56 -0600

    try - chach

--- default/app/controllers/recibos_controller.php
+++ default/app/controllers/recibos_controller.php
@@ -16,35 +16,32 @@ class RecibosController extends AppController
 
     public function crearRecibo()
     {
-
         View::select(null);
         View::template(null);
 
-        $recibo = new Recibo(Input::post('recibo'));
+        try {
+            $recibo = new Recibo(Input::post('recibo'));
+            $paths = (new Recibo())->crearRecibo(
+                $recibo
+            );
 
-        $paths = (new Recibo())->crearRecibo(
-            $recibo
-        );
+            if (file_exists($paths ['fullPathSavedContract'])) {
+                header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
+                header("Cache-Control: public");
+                header("Content-Type: application/octet-stream");
+                header("Content-Transfer-Encoding: Binary");
+                header("Content-Length:".filesize($paths['fullPathSavedContract']));
+                header("Content-Disposition: attachment; filename={$paths['fullNameSavedContract']}");
+                readfile($paths['fullPathSavedContract']);
 
-        if (file_exists($paths ['fullPathSavedContract'])) {
-            header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
-            header("Cache-Control: public");
-            header("Content-Type: application/octet-stream");
-            header("Content-Transfer-Encoding: Binary");
-            header("Content-Length:".filesize($paths['fullPathSavedContract']));
-            header("Content-Disposition: attachment; filename={$paths['fullNameSavedContract']}");
-            readfile($paths['fullPathSavedContract']);
-
-        } else {
-            header($_SERVER["SERVER_PROTOCOL"] . " 500 ERROR");
-            header("Cache-Control: public");
-            header("Content-Type: application/json");
-            $this->data = ['ok' => false];
+            } else {
+                header($_SERVER["SERVER_PROTOCOL"] . " 500 ERROR");
+                header("Cache-Control: public");
+                header("Content-Type: application/json");
+                $this->data = ['ok' => false];
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
         }
-
-
-
-
-
     }
 }
