2d3ddc6d3df0bca99d928ae875f63efc418c7710
AlfredoVientoGonzalez <alfredo.gonzalez@vientodigital.com>
2023-08-26 16:40:24 -0600

    cambios de ui y js

--- default/public/javascript/functions.js
+++ default/public/javascript/functions.js
@@ -4,6 +4,64 @@ function clearInputs ()
     $('input[type=number]').each( (i,input) => $(input).val(null) );
 }
 
+
+
+function checkFormIsValid() {
+
+ checkInvalidInputs();
+ checkValidForm();
+
+}
+
+
+function checkInvalidInputs() {
+  $('input[required]')
+  .each( 
+    (i,input) => {
+      if(input.value <= 0 ){
+        swal
+          .fire(
+            {
+              title: '<strong>Error</strong>',
+              icon: 'error',
+              html:
+                'El campo <b> <u>'+input.name+'</u></b> es obligatorio ' ,
+              showCloseButton: false,
+            }
+          );
+          return 0;
+        }
+        return 1;
+      }
+    );
+}
+
+function checkValidForm( isUnRequiredField ) {
+  var valid = true;
+  $('input[required]')
+  .each( 
+    (i,input) => {
+      if(input.value <= 0 ){
+        valid = false
+      }
+    }
+  );
+  
+  if (valid) {
+    swal
+    .fire(
+      {
+        title: '<strong>Correcto</strong>',
+        icon: 'success',
+        html:
+          'Generando documento por favor espere' ,
+        showCloseButton: false,
+      }
+    );
+  }
+  
+}
+
 if(window.location.href.includes('localhost')){
     $(document).ready(function() {
         $('li.active').removeClass('active').removeAttr('aria-current');
