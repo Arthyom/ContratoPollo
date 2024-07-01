17ab2b0b94860323fe22813bcb747263293d02cb
Alfredo Gonzalez Gaviña <alfredogonzalez@ugto.mx>
2024-06-30 19:53:46 -0600

    Agregando elementos nuevos

--- default/app/controllers/propiedades_controller.php
+++ default/app/controllers/propiedades_controller.php
@@ -5,7 +5,7 @@ class PropiedadesController extends ScaffoldController
     public $model = 'propiedades';
     public $scaffild = 'kumbia';
 
-    public function index($page = 1)
+    public function index($page = 1, $orderBy = '')
     {
         $this->data = (new $this->model())->paginate("page: $page", "order: Nombre asc", "per_page: 15");
     }
